4c5fe605294bed27b03f1b3e5dbfec436f33d3d9
jaeho.lee <jaeho.lee@bespinglobal.com>
2024-04-24 15:05:03 +0900

    test 패키지 주석처리..

--- src/test/kotlin/com/saas/bff/BffApplicationTests.kt
+++ src/test/kotlin/com/saas/bff/BffApplicationTests.kt
@@ -3,7 +3,7 @@ package com.saas.bff
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
+/*@SpringBootTest*/
 class BffApplicationTests {
 
     @Test
