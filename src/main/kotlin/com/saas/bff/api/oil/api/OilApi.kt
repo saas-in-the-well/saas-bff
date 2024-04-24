ee4b406f401bfddb3a5527f82c1a641ef965d9b6
jaeho.lee <jaeho.lee@bespinglobal.com>
2024-04-24 14:43:43 +0900

    Dockerfile 추가

--- src/main/kotlin/com/saas/bff/api/oil/api/OilApi.kt
+++ src/main/kotlin/com/saas/bff/api/oil/api/OilApi.kt
@@ -1,6 +1,5 @@
 package com.saas.bff.api.oil.api
 
-import com.saas.bff.model.RequestModel
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
@@ -12,8 +11,8 @@ class OilApi (
 ){
 
     fun getLowTop10(area: String, cnt: String, prodcd: String): Mono<String> {
-
-        return saasOilWebClient.get()
+        return saasOilWebClient
+            .get()
             .uri("/opinet/api/lowTop10?area=$area&cnt=$cnt&prodcd=$prodcd")
             .retrieve()
             .bodyToMono(String::class.java)
